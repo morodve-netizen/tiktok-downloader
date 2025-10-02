4996470ae8b80713257a8403cade469edd0a56cc
morodve-netizen <morodve@gmail.com>
2025-10-02 17:57:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Telegram = require('node-telegram-bot-api');
 var request = require("request");
-var token = 'YOUR_TELEGRAM_BOT_TOKEN';
+var token = process.env.TOKEN;
 
 // Configure the bot to use polling
 var opt = {
